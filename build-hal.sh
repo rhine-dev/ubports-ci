b27c56f9fa2bfd2c26a66e673ed8735d72d4c890
Konrad Dybcio <konradybcio@gmail.com>
2020-04-08 23:42:05 +0200

    Set up builds for Sony Xperia Z1

--- build-hal.sh
+++ build-hal.sh
@@ -4,12 +4,10 @@ cd $ANDROID_ROOT
 
 source build/envsetup.sh
 export USE_CCACHE=1
-breakfast $DEVICE
+lunch $LUNCH_DEVICE
 make -j$(nproc) halium-boot
-make -j$(nproc) recoveryimage
 make -j$(nproc) systemimage 
 
 echo "md5sum halium-boot.img and system.img"
-md5sum $ANDROID_ROOT/out/target/product/santoni/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/santoni/recovery.img
-md5sum $ANDROID_ROOT/out/target/product/santoni/system.img
+md5sum $ANDROID_ROOT/out/target/product/honami/halium-boot.img
+md5sum $ANDROID_ROOT/out/target/product/honami/system.img
